8d906af486513fec2990188dc65b2c217f42d1a3
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-04-18 23:31:58 +0300

    vtc-members.php

--- vtc-members.php
+++ vtc-members.php
@@ -1,9 +1,17 @@
 <?php
+// CORS izin ver
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+// VTC ID
 $vtcId = "59338";
+
+// TruckersMP API URL'si
 $url = "https://api.truckersmp.com/v2/vtc/$vtcId/members";
+
+// API'den veriyi al
 $response = file_get_contents($url);
+
+// Gelen veriyi aynen döndür
 echo $response;
 ?>
